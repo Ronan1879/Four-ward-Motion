28c2b8290e7d654dde9c9fd6fa3a1fb74787c849
Sabrina Berger <sabrinaberger@berkeley.edu>
2019-11-02 20:20:26 -0400

    tracking particles -SB

--- Simulation_files/universe.py
+++ Simulation_files/universe.py
@@ -8,6 +8,7 @@ import stddraw
 from body import Body 
 from instream import InStream
 from vector import Vector
+import numpy as np
 
 #-----------------------------------------------------------------------
 
@@ -19,10 +20,14 @@ class Universe:
     def __init__(self, filename):
         instream = InStream(filename)
         n = instream.readInt()
+
         radius = instream.readFloat()
         stddraw.setXscale(-radius, +radius)
         stddraw.setYscale(-radius, +radius)
         self._bodies = stdarray.create1D(n)
+        self._xtracks = stdarray.create1D(n)
+        self._ytracks = stdarray.create1D(n)
+
         for i in range(n):
             rx   = instream.readFloat()
             ry   = instream.readFloat()
@@ -32,6 +37,8 @@ class Universe:
             r = Vector([rx, ry])
             v = Vector([vx, vy])
             self._bodies[i] = Body(r, v, mass)
+            self._xtracks[i] = []
+            self._ytracks[i] = []
 
     # Simulate the passing of dt seconds in self.
     
@@ -51,12 +58,21 @@ class Universe:
 
         # Move the bodies.
         for i in range(n):
-            self._bodies[i].move(f[i], dt)    
+            current_body = self._bodies[i]
+            current_body.move(f[i], dt)  
+            self._xtracks[i].append(current_body._r[0])
+            self._ytracks[i].append(current_body._r[1])
+            # print(n)
+            if n == 10:
+                np.save("xtracks.npy", self._xtracks)
+                np.save("ytracks.npy", self._ytracks)
+
 
     # Draw self to standard draw.
     def draw(self):
         for body in self._bodies:
             body.draw()
+
 
 #-----------------------------------------------------------------------
 
@@ -69,11 +85,16 @@ def main():
     filename = sys.argv[1]
     dt = float(sys.argv[2])
     universe = Universe(filename)
+    j = 0 
     while True:
+        j += 1
         universe.increaseTime(dt)
-        #stddraw.clear()
+        stddraw.clear()
         universe.draw()
         stddraw.show(10)
+        if j == 1000:
+            np.save("xtracks_{}.npy".format(len(universe._xtracks)), universe._xtracks)
+            np.save("ytracks_{}.npy".format(len(universe._ytracks)), universe._ytracks)
 
 if __name__ == '__main__':
     main()
